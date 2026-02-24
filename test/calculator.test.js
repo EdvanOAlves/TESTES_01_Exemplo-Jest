31d7c6c9b2e25e49c9e29e453d9782d9010fa68f
EDVAN ALVES DE OLIVEIRA <25132698@CFP127ALUNO.LOCAL>
2026-02-24 08:18:41 -0300

    test: init jest dependency and calculator test file

---
+++ test/calculator.test.js
@@ -0,0 +1,8 @@
+/*******************************************************************************************
+ * Objective: This file is assigned to execute Unitary Tests on calculator.js. We'll validate
+ * each and every function created on that file
+ * Date: 24/02/2026
+ * Author: Edvan
+ * Version 1.0
+ * 
+*******************************************************************************************/
