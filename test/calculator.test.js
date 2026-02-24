92cd25707ea5209dd4c3d84c641c7177ae374003
EDVAN ALVES DE OLIVEIRA <25132698@CFP127ALUNO.LOCAL>
2026-02-24 09:46:06 -0300

    test: complete initial tests

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -26,3 +26,30 @@ test('Validation of data input for a number:', function(){
     expect(mathCalculator.verifyData('5')).toBe(true)
     expect(mathCalculator.verifyData('5.6')).toBe(true)
 })
+
+// SCENARIO 02: Testing validation for SUM operation
+test('Validation of SUM with two numbers', function(){
+    //Valid inputs
+    expect (mathCalculator.calculate(10,30, 'sum')).toBe(40)
+    expect (mathCalculator.calculate(10,30, 'somar')).toBe(40)
+    expect (mathCalculator.calculate('10', '30','sum')).toBe(40)
+    expect (mathCalculator.calculate('10', '30','somar')).toBe(40)
+    expect (mathCalculator.calculate(5.6, 3.2, 'sum')).toBe(8.8)
+    expect (mathCalculator.calculate('5.6', '3.2','sum')).toBe(8.8)
+    expect (mathCalculator.calculate(10, 30,'SoMaR')).toBe(40)
+    
+    //Invalid inputs
+    expect (mathCalculator.calculate(10, 30,'soma')).toBe(false)
+})
+
+// SCENARIO 03: Testing validation for SUBTRACTION operation
+test('Validation of SUBTRACT with two numbers', function(){
+    //Valid inputs
+    expect (mathCalculator.calculate(10,30, 'subtract')).toBe(-20)
+    expect (mathCalculator.calculate(40,30, 'subtract')).toBe(10)
+    expect (mathCalculator.calculate(5.6, 4.2,'subtract')).toBe(1.4)
+    expect (mathCalculator.calculate('10', '30','subTRAIR')).toBe(-20)
+
+    //Invalid inputs
+    expect (mathCalculator.calculate(10, 30,'subtrai aí meu')).toBe(false)
+})
