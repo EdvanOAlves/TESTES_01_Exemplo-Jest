771f76552a4011dfeb381444f47e1b3cc4973d5f
EDVAN ALVES DE OLIVEIRA <25132698@CFP127ALUNO.LOCAL>
2026-02-24 08:47:10 -0300

    test: add test scenario 01

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -12,3 +12,17 @@
 const mathCalculator = require('../model/calculator.js')
 
 // SCENARIO 01: Testing data Verification
+test('Validation of data input for a number:', function(){
+    //Invalid inputs
+    expect(mathCalculator.verifyData(null)).toBe(false)
+    expect(mathCalculator.verifyData(undefined)).toBe(false)
+    expect(mathCalculator.verifyData('a')).toBe(false)
+    expect(mathCalculator.verifyData('2a')).toBe(false)
+    expect(mathCalculator.verifyData('4,5')).toBe(false)
+    
+    //Valid inputs
+    expect(mathCalculator.verifyData(5)).toBe(true)
+    expect(mathCalculator.verifyData(5.6)).toBe(true)
+    expect(mathCalculator.verifyData('5')).toBe(true)
+    expect(mathCalculator.verifyData('5.6')).toBe(true)
+})
